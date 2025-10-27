87af77e562444efb03c53aad30db79de3ed6b3b7
Alesandra Silva <alesandra.a.silva@aluno.senai.br>
2025-10-27 14:28:43 -0300

    Rota principal feita

--- main.py
+++ main.py
@@ -13,3 +13,17 @@ templetes = Jinja2Templetes(directory="templetes")
 
 #Definir a pasta onde os arquivos estaticos (CSS, Imagem e JS)
 app.mount("/static",StaticFiles(directory="static"), name="static")
+alunos = [
+    {"nome": "Iago", "nota": 8.5}
+    {"nome": "Murilo", "nota" 6.5}
+    {"nome": "joana", "nota" 9.5}
+    {"nome": "Francisco", "nota" 8.0}
+]
+
+#Rota inicial
+@app.get("/", response_class=HTMLResponse)
+def home(requests : Requests):
+    return templetes.Templetesresponse(
+        "index.html",
+        {"requests": requests, "lista_alunos": alunos}
+    )

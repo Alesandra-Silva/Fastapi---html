37a75585b215af7fe74dd8ff84bd8436dd09f79d
Alesandra Silva <alesandra.a.silva@aluno.senai.br>
2025-10-27 16:28:30 -0300

    rota inicial e index feito

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 # pip install fastapi uvicorn jinja2 python-multipart
 
-from fastapi import FastAPi, Requests, RedirectResponse
-from fastapi.response import HTMLResponse, RedirectResponse
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles 
-from fastapi.templating import Jinja2Templetes
+from fastapi.templating import Jinja2Templates
 
-app = FastAPi(title="Lista de alunos")
+# python -m uvicorn main:app --reload
+
+app = FastAPI(title="Lista de alunos")
 
 
 #Definir a pasta onde está os html
-templetes = Jinja2Templetes(directory="templetes")
+templetes = Jinja2Templates(directory="templetes")
 
 #Definir a pasta onde os arquivos estaticos (CSS, Imagem e JS)
 app.mount("/static",StaticFiles(directory="static"), name="static")
 alunos = [
-    {"nome": "Iago", "nota": 8.5}
-    {"nome": "Murilo", "nota" 6.5}
-    {"nome": "joana", "nota" 9.5}
-    {"nome": "Francisco", "nota" 8.0}
+    {"nome": "Iago", "nota": 8.5},
+    {"nome": "Murilo", "nota": 6.5},
+    {"nome": "Joana", "nota" : 9.5},
+    {"nome": "Francisco", "nota": 8.0}
 ]
 
 #Rota inicial
 @app.get("/", response_class=HTMLResponse)
-def home(requests : Requests):
-    return templetes.Templetesresponse(
+def home(request : Request):
+    return templetes.TemplateResponse(
         "index.html",
-        {"requests": requests, "lista_alunos": alunos}
+        {"request": request, "lista_alunos": alunos}
     )
